19c8d7b28ace0a777053f44e9d2af7adb96f333d
Lars Brinkhoff <lars@nocrew.org>
2017-09-12 20:08:43 +0200

    Update lbForth version.

--- test/deps.sh
+++ test/deps.sh
@@ -1,7 +1,7 @@
 install_lbforth() {
     cd lbForth
     export M32=-m32
-    sh -e install-deps.sh install_${TRAVIS_OS_NAME:-linux}
+    sh -e test/install-deps.sh install_${TRAVIS_OS_NAME:-linux}
     make all TARGET=x86 OS=linux
     sudo make install TARGET=x86 OS=linux
 }
